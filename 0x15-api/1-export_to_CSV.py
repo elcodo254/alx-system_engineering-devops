55cfc071e4ae6a33e8ba61992061a1cc0cce5cd0
elcodo254 <ellawachaga@gmail.com>
2023-05-22 17:48:28 -0700

    code

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Using REST api exports to-do list for employees given an id, in CSV format
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    user_name = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    with open("{}.csv".format(sys.argv[1]), "w", newline="") as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        [writer.writerow([sys.argv[1], username, todo.get(
+        "completed"), todo.get("title")]) for todo in todos]
