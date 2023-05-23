5c27353bbcfbb25afbb0aad1bdc7430a684aba1c
elcodo254 <ellawachaga@gmail.com>
2023-05-22 17:14:34 -0700

    code

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Using REST api returns to-do list for employees given an id"""
+import requests
+import sy
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    inall = [todo.get("title") for todo in todos if todo.get(
+        "completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(user.get(
+        "name"), len(inall) len(todos)))
+    [print("\t {}".format(i)) for i in inall]
