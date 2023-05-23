7f23a23ac9fd6e874ab1b00fd8d8370907e0a277
elcodo254 <ellawachaga@gmail.com>
2023-05-22 18:07:43 -0700

    code

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Using REST api exports to-do list for employees given an id, in json format
+"""
+import json 
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    with open("{}.json".format(sys.argv[1]), "w") as json_file:
+        json.dump({sys.argv[1]: [{"task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": username} for todo in todos]}, json_file)
